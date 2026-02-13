c94eb83ab85279a5843c59a11008fc3560ca8a62
DeveloperSyamKumar <155809208+DeveloperSyamKumar@users.noreply.github.com>
2026-02-14 00:48:30 +0530

    HIDDEN DEVELOPER DETAILS.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -71,7 +71,7 @@ const Footer = () => {
           </p> */}
         </div>
 
-        {/* Developer Details */}
+        /* {/* Developer Details */}
         <div>
           <h2 className="text-base font-semibold text-blue-900 mb-2">Developer</h2>
           <p className="text-gray-700">
@@ -93,7 +93,7 @@ const Footer = () => {
             </a>
           </p>
         </div>
-      </div>
+      </div> */
 
       {/* Bottom Section */}
       <div className="border-t border-blue-300 py-2 text-center text-xs text-gray-700">
